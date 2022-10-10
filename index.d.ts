aa20eaf4b0de69730cc94cbc2729036bd3789a1f
Hayden Bleasel <hello@haydenbleasel.com>
2022-10-10 15:14:38 -0400

    Rewrite extension.ts, improve client-api types

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,54 @@
-declare module '@corellium/client-api';
+/* eslint-disable import/unambiguous */
+
+declare module '@corellium/client-api' {
+  export const ApiClient: {
+    instance: {
+      authentications: {
+        BearerAuth: {
+          accessToken: string;
+        };
+      };
+      basePath: string;
+    };
+  };
+  export class CorelliumApi {
+    public v1StartInstance(
+      instanceUUID: string,
+      opts?: Record<string, unknown>
+    ): Promise<never>;
+    public v1StopInstance(
+      instanceUUID: string,
+      opts?: Record<string, unknown>
+    ): Promise<never>;
+    public v1RebootInstance(
+      instanceUUID: string,
+      opts?: Record<string, unknown>
+    ): Promise<never>;
+    public v1GetInstanceConsole(
+      instanceUUID: string,
+      opts?: Record<string, unknown>
+    ): Promise<{
+      url?: string;
+    }>;
+    public v1CreateSnapshot(
+      instanceUUID: string,
+      opts?: Record<string, unknown>
+    ): Promise<{
+      id?: string;
+    }>;
+    public v1GetSnapshots(
+      instanceUUID: string,
+      opts?: Record<string, unknown>
+    ): Promise<
+      {
+        name: string;
+        id: string;
+      }[]
+    >;
+    public v1RestoreSnapshot(
+      instanceUUID: string,
+      snapshotId: string,
+      opts?: Record<string, unknown>
+    ): Promise<never>;
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,148 +1,176 @@
-import * as vscode from 'vscode';
-import * as corelliumClient from '@corellium/client-api';
+import { ApiClient, CorelliumApi } from '@corellium/client-api';
+import type { ExtensionContext, QuickPickItem } from 'vscode';
+import { commands, env, Uri, window, workspace } from 'vscode';
+import CorelliumConsole from './lib/console';
+import type CorelliumInstance from './lib/corelliumInstance';
+import parseError from './lib/parseError';
+import VirtualDevicesProvider from './lib/virtualDevicesProvider';
 
-let apiInstance: any = null;
+let apiInstance: CorelliumApi | null = null;
 
-export const activate = (context: vscode.ExtensionContext) => {
+export const activate = (context: ExtensionContext): void => {
   // Set up the API client object, note that apiInstance is global
-  const configuration = vscode.workspace.getConfiguration('corellium');
-  const defaultClient = corelliumClient.ApiClient.instance;
+  const configuration = workspace.getConfiguration('corellium');
+  const defaultClient = ApiClient.instance;
   const endpoint = configuration.get('endpoint');
-  defaultClient.basePath = `${endpoint}/api`;
+  const apiKey = configuration.get('ApiKey');
   const bearerAuth = defaultClient.authentications.BearerAuth;
-  bearerAuth.accessToken = configuration.get('ApiKey');
-  apiInstance = new corelliumClient.CorelliumApi();
+
+  // Overwrite the default endpoint and bearer auth
+  if (typeof endpoint === 'string') {
+    defaultClient.basePath = `${endpoint}/api`;
+  }
+  if (typeof apiKey === 'string') {
+    bearerAuth.accessToken = apiKey;
+  }
+
+  apiInstance = new CorelliumApi();
 
   // Set up tree view for projects/devices
   const virtualDevicesProvider = new VirtualDevicesProvider();
-  vscode.window.registerTreeDataProvider(
-    'virtualDevices',
-    virtualDevicesProvider
-  );
+  window.registerTreeDataProvider('virtualDevices', virtualDevicesProvider);
 
   // Command handlers
-  const turnOnDeviceCommandHandler = (instance: CorelliumInstance) => {
-    apiInstance.v1StartInstance(instance.instanceUUID, null).then(
-      () => {
-        vscode.window.showInformationMessage(`Turning on ${instance.label}`);
-      },
-      (error: Error) => {
-        vscode.window.showErrorMessage(
-          `Failed to turn on device: ${error.message}`
-        );
-      }
-    );
+  const turnOnDeviceCommandHandler = async (instance: CorelliumInstance) => {
+    try {
+      await apiInstance?.v1StartInstance(instance.instanceUUID);
+      await window.showInformationMessage(`Turning on ${instance.label}`);
+    } catch (error) {
+      const message = parseError(error);
+      await window.showErrorMessage(`Failed to turn on device: ${message}`);
+    }
   };
 
-  const turnOffDeviceCommandHandler = (instance: CorelliumInstance) => {
-    apiInstance.v1StopInstance(instance.instanceUUID, null).then(
-      () => {
-        vscode.window.showInformationMessage(`Turning off ${instance.label}`);
-      },
-      (error: Error) => {
-        vscode.window.showErrorMessage(
-          `Failed to turn off device: ${error.message}`
-        );
-      }
-    );
+  const turnOffDeviceCommandHandler = async (instance: CorelliumInstance) => {
+    try {
+      await apiInstance?.v1StopInstance(instance.instanceUUID);
+      await window.showInformationMessage(`Turning off ${instance.label}`);
+    } catch (error) {
+      const message = parseError(error);
+      await window.showErrorMessage(`Failed to turn off device: ${message}`);
+    }
   };
 
-  const rebootDeviceCommandHandler = (instance: CorelliumInstance) => {
-    apiInstance.v1RebootInstance(instance.instanceUUID).then(
-      () => {
-        vscode.window.showInformationMessage(`Rebooting ${instance.label}`);
-      },
-      (error: Error) => {
-        vscode.window.showErrorMessage(
-          `Failed to reboot device: ${error.message}`
-        );
-      }
-    );
+  const rebootDeviceCommandHandler = async (instance: CorelliumInstance) => {
+    try {
+      await apiInstance?.v1RebootInstance(instance.instanceUUID);
+      await window.showInformationMessage(`Rebooting ${instance.label}`);
+    } catch (error) {
+      const message = parseError(error);
+      await window.showErrorMessage(`Failed to reboot device: ${message}`);
+    }
   };
 
-  const openInBrowserCommandHandler = (instance: CorelliumInstance) => {
-    const url = `${endpoint}/devices/${instance.instanceUUID}/connect`;
-    vscode.env.openExternal(vscode.Uri.parse(url));
+  const openInBrowserCommandHandler = async (instance: CorelliumInstance) => {
+    const parsedEndpoint =
+      typeof endpoint === 'string' ? endpoint : 'https://app.corellium.com';
+    const url = new URL(
+      `devices/${instance.instanceUUID}/connect`,
+      parsedEndpoint
+    );
+    const external = Uri.parse(url.href);
+
+    try {
+      await env.openExternal(external);
+    } catch (error) {
+      const message = parseError(error);
+      await window.showErrorMessage(
+        `Failed to open device in browser: ${message}`
+      );
+    }
   };
 
   // const writeEmitter = new vscode.EventEmitter<string>();
   const openConsoleCommandHandler = async (instance: CorelliumInstance) => {
-    const consoleResult = await apiInstance.v1GetInstanceConsole(
-      instance.instanceUUID
-    );
-    const consoleWSUrl = consoleResult.url;
-    const pty = new CorelliumConsole(consoleWSUrl);
-    const terminal = vscode.window.createTerminal({
-      name: `Console: ${instance.label}`,
-      pty,
-    });
-    terminal.show();
+    try {
+      const consoleResult = await apiInstance?.v1GetInstanceConsole(
+        instance.instanceUUID
+      );
+      const consoleWSUrl = consoleResult?.url;
+
+      if (!consoleWSUrl) {
+        throw new Error('No console URL returned');
+      }
+
+      const pty = new CorelliumConsole(consoleWSUrl);
+      const terminal = window.createTerminal({
+        name: `Console: ${instance.label}`,
+        pty,
+      });
+
+      terminal.show();
+    } catch (error) {
+      const message = parseError(error);
+      await window.showErrorMessage(
+        `Failed to open device console: ${message}`
+      );
+    }
   };
 
   const takeSnapshotCommandHandler = async (instance: CorelliumInstance) => {
-    const snapshotName = await vscode.window.showInputBox({
-      placeHolder: 'My New Snapshot',
-      prompt: 'Snapshot Name:',
-      value: 'My New Snapshot',
-    });
-    const snapshotCreationOptions = {
-      name: snapshotName,
-    };
-    apiInstance
-      .v1CreateSnapshot(instance.instanceUUID, snapshotCreationOptions)
-      .then(
-        (data: { id: string }) => {
-          vscode.window.showInformationMessage(
-            `Snapshotting ${instance.label}: ${data.id}`
-          );
-        },
-        (error: Error) => {
-          vscode.window.showErrorMessage(
-            `Failed to take snapshot: ${error.message}`
-          );
-        }
+    try {
+      const snapshotName = await window.showInputBox({
+        placeHolder: 'My New Snapshot',
+        prompt: 'Snapshot Name:',
+        value: 'My New Snapshot',
+      });
+      const snapshotCreationOptions = {
+        name: snapshotName,
+      };
+
+      const data = await apiInstance?.v1CreateSnapshot(
+        instance.instanceUUID,
+        snapshotCreationOptions
       );
+
+      if (!data?.id) {
+        throw new Error('No snapshot ID returned');
+      }
+
+      await window.showInformationMessage(
+        `Snapshotting ${instance.label}: ${data.id}`
+      );
+    } catch (error) {
+      const message = parseError(error);
+      await window.showErrorMessage(`Failed to take snapshot: ${message}`);
+    }
   };
 
   const restoreSnapshotCommandHandler = async (instance: CorelliumInstance) => {
-    const items: vscode.QuickPickItem[] = [];
-    let snapshots = null;
+    const items: QuickPickItem[] = [];
 
     try {
-      snapshots = await apiInstance.v1GetSnapshots(instance.instanceUUID);
-    } catch (error: any) {
-      vscode.window.showErrorMessage(
-        `Failed to fetch snapshots: ${error.message}`
+      const snapshots = await apiInstance?.v1GetSnapshots(
+        instance.instanceUUID
       );
-      return;
-    }
 
-    for (let index = 0; index < snapshots.length; index++) {
-      const item = snapshots[index];
-      items.push({
-        label: item.name,
-        description: item.id,
-      });
-    }
+      snapshots?.map((snapshot) => ({
+        label: snapshot.name,
+        description: snapshot.id,
+      }));
 
-    const selection = await vscode.window.showQuickPick(items);
-    if (!selection) {
-      // User cancelled
-      return;
-    }
+      const selection = await window.showQuickPick(items);
 
-    try {
-      await apiInstance.v1RestoreSnapshot(
+      if (!selection) {
+        // User cancelled
+        return;
+      }
+
+      if (!selection.description) {
+        throw new Error('No snapshot ID returned');
+      }
+
+      await apiInstance?.v1RestoreSnapshot(
         instance.instanceUUID,
         selection.description
       );
-      vscode.window.showInformationMessage(
+
+      await window.showInformationMessage(
         `Restoring snapshot ${selection.label} on ${instance.label}`
       );
-    } catch (error: any) {
-      vscode.window.showErrorMessage(
-        `Failed to restore snapshot: ${error.message}`
-      );
+    } catch (error) {
+      const message = parseError(error);
+      await window.showErrorMessage(`Failed to restore snapshot: ${message}`);
     }
   };
 
@@ -151,49 +179,46 @@ export const activate = (context: vscode.ExtensionContext) => {
   };
 
   context.subscriptions.push(
-    vscode.commands.registerCommand(
+    commands.registerCommand(
       'corellium.startDevice',
       turnOnDeviceCommandHandler
     )
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand(
+    commands.registerCommand(
       'corellium.stopDevice',
       turnOffDeviceCommandHandler
     )
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand(
+    commands.registerCommand(
       'corellium.rebootDevice',
       rebootDeviceCommandHandler
     )
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand(
+    commands.registerCommand(
       'corellium.openInBrowser',
       openInBrowserCommandHandler
     )
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand(
-      'corellium.openConsole',
-      openConsoleCommandHandler
-    )
+    commands.registerCommand('corellium.openConsole', openConsoleCommandHandler)
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand(
+    commands.registerCommand(
       'corellium.takeSnapshot',
       takeSnapshotCommandHandler
     )
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand(
+    commands.registerCommand(
       'corellium.restoreSnapshot',
       restoreSnapshotCommandHandler
     )
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand(
+    commands.registerCommand(
       'corellium.refreshDevices',
       refreshDevicesCommandHandler
     )
@@ -201,4 +226,7 @@ export const activate = (context: vscode.ExtensionContext) => {
 };
 
 // this method is called when your extension is deactivated
-export const deactivate = () => {};
+export const deactivate = (): void => {
+  // eslint-disable-next-line no-console
+  console.log('Deactivating...');
+};
